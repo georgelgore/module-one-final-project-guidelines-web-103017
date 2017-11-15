8fa1728ec0dd9fe0ce4664c0cf985db26661521c
George Gore <georgelgore@gmail.com>
2017-11-15 16:07:06 -0500

    seeded tables

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,12 @@
 require 'bundler'
 Bundler.require
 
-ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
-require_all 'lib'
+# ActiveRecord::Base.connection.execute("BEGIN TRANSACTION; END;")
+db = ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
+# db.busy_timeout=(100)
+ # db = ::SQLite3::Database.new(@opts[:database])
+# db.busy_timeout(5000)=> Card
+# sqlite3_busy_timeout( db, 5000 );
 
+require_all 'lib'
 require_all 'app'
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,22 +2,40 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+  # Card.destroy_all
     # for seed
     # (1..311).each do |page_num|
     #       url = 'https://api.magicthegathering.io/v1/cards?page=#{page_num}'
           # response = RestClient.get(url)
           # cards_hash = JSON.parse(response)
           # cards_hash["cards"].each do |card_hash|
-    (1..311).each do |page_num|
+    (251..311).each do |page_num|
 
       url = "https://api.magicthegathering.io/v1/cards?page=#{page_num}"
       response = RestClient.get(url)
       cards = JSON.parse(response)
       cards["cards"].each do |card_hash|
-        binding.pry
 
+        name = card_hash['name']
+        manaCost = card_hash['manaCost']
+        cmc = card_hash['cmc']
+        colors = card_hash['colors']
+        types = card_hash['types']
+        subtypes = card_hash['subtypes']
+        text = card_hash['text']
+        power = card_hash['power'].to_i
+        toughness = card_hash['toughness'].to_i
+        Card.create(name: name, manaCost: manaCost, cmc: cmc, colors: colors, types: types, subtypes: subtypes, text: text, power: power, toughness: toughness)
       end
-
 
     end
   # End loop
+  # t.string :name
+  # t.string :manaCost
+  # t.integer :cmc
+  # t.string :colors
+  # t.string :types
+  # t.string :subtypes
+  # t.text :text
+  # t.integer :power
+  # t.integer :toughness
