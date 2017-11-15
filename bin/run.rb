1fe63d99751b0145c633bcd2fe8efc13817b7ca7
George Gore <georgelgore@gmail.com>
2017-11-15 09:30:15 -0500

    add api connector

--- bin/run.rb
+++ bin/run.rb
@@ -1,2 +1,3 @@
 require_relative 'config/environment'
-
+require_relative '../lib/api_communicator.rb'
+require_relative '../lib/config/environment.rb'
---
+++ lib/api_communicator.rb
@@ -0,0 +1,31 @@
+require 'rest-client'
+require 'json'
+require 'pry'
+# require 'mtg_sdk'
+
+def get_cards_from_api
+  url = 'https://api.magicthegathering.io/v1/cards'
+  response = RestClient.get(url)
+  recipe_hash = JSON.parse(response)
+  binding.pry
+  
+end
+
+# cards = MTG::Card.where(name: 'avacyn').all
+get_cards_from_api
+
+
+#
+# def parse_character_movies(films_hash)
+#   # some iteration magic and puts out the movies in a nice list
+# end
+#
+# def show_character_movies(character)
+#   films_hash = get_character_movies_from_api(character)
+#   parse_character_movies(films_hash)
+# end
+
+## BONUS
+
+# that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
+# can you split it up into helper methods?
