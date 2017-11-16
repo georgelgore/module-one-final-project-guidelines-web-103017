c80567de5dc13b6ef8b0348d8e948433883d0860
github username <github email address>
2017-11-16 11:42:30 -0500

    Added search by name

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -13,13 +13,16 @@ end
 
 # Provide search options
 def main_menu
+  puts "Main Menu:"
   puts "  1. Search Cards"
   puts "  2. Build a Deck"
+  puts ""
+  puts "  Type 'exit' to quit"
   input = gets.chomp
   main_menu_reader(input)
 end
 
-# Read what input
+# Read user input from main menu
 def main_menu_reader(input)
   case input
   when "1"
@@ -27,57 +30,91 @@ def main_menu_reader(input)
     #call search cards menu
   when "2"
     # call deck builder menu
+  when "exit"
+    exit
   else
     puts "Input not recognized, please enter a number between 1 and 4."
     main_menu
   end
 end
 
+# Display the search cards menu
 def search_cards_menu
   puts "------------------"
   puts "Search Cards Menu:"
-  puts "  1. Search by Name"
-  puts "  2. Search by Multiple Attributes"
-  puts "  3. Return to Main Menu"
+  puts "  -1. Search by Name"
+  puts "  -2. Search by Multiple Attributes"
+  puts "  -3. Return to Main Menu"
+  puts ""
+  puts "  Type 'exit' to quit"
   input = gets.chomp
   search_cards_menu_reader(input)
 end
 
+# Read user input from search cards menu
 def search_cards_menu_reader(input)
+  case input
+  when "1"
+    search_cards_by_name
+  when "2"
+    # call search cards by multiple attributes
+  when "3"
+    puts "------------------"
+    main_menu
+  when "exit"
+    exit
+  else
+    puts "Input not recognized, please enter a number between 1 and 3."
+    search_cards_menu
+  end
+end
 
-
+# Search cards by name
+def search_cards_by_name
+  puts "~~~~~~~~~~~~~~~~~~"
+  puts "Type the name of the card that you wish to find"
+  input = gets.chomp
+  card = search_by_name(input)
 end
 
 
 
-#### Deck ####
-def build_deck_submenu
-  puts "Welcome to Deckbuilder"
-  puts "1. Create a Deck"
-  puts "Option 2"
+
+
+
+#### SEARCHES ####
+
+def search_by_name(input)
+  card = Card.find_by(name: input)
+  #binding.pry
+  puts "Name: #{card.name}"
+  puts "Mana Cost: #{card.manaCost}"
+  puts "Converted Mana Cost: #{card.cmc}"
+  attr_duplicate?(card.color1, card.color2, "Color")
+  puts "Type: #{card.types}"
+  attr_duplicate?(card.subtype1, card.subtype2, "Subtype")
+  puts "#{card.power}/#{card.toughness}"
+
+  save_searched_card(card)
+end
+
+def save_searched_card(card)
+  puts "Would you like to save #{card.name}?"
+  puts "    --1. Yes"
+  puts "    --2. No"
+  input = gets.chomp
+  save_searched_card_reader(input)
 end
 
 
-def create_a_deck(name)
-end
+##### HELPER #####
 
 
-
-#### Get Input ####
-
-# gets.chomp the card name
-def get_name_from_user
-  puts "please enter a card name"
-  name = gets.chomp
-  name
-end
-
-
-
-
-
-
-##### UNDER DEVELOPMENT #####
-def under_development
-  puts "Option under development!"
+def attr_duplicate?(attr1, attr2, attribute)
+  #binding.pry
+  if attr1 == attr2 || attr2.nil?
+     puts "#{attribute}: #{attr1}"
+  elsif
+    puts "#{attribute}s: #{attr1}, #{attr2}"
+  end
 end
