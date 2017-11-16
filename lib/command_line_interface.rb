c606542c7a7f9f73dda1090cfc08fb1b1321521c
github username <github email address>
2017-11-16 10:35:37 -0500

    added main menu user create/find

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,12 +1,40 @@
-def welcome
-  # puts out a welcome message here!
-  puts "Welcome!"
-end
+#### WELCOME METHODS ####
+
+
 
 def main_menu
   puts "Welcome to Gathered, a Magic the Gathering Deckbuilder"
-  puts "Please select a number:"
+  puts "Please tell us your full name:"
+  full_name = gets.chomp
+  user = User.find_or_create_by(name: full_name)
+  puts "Hello, #{user.name}"
 end
+
+
+
+
+#### Deck ####
+def build_deck_submenu
+  puts "Welcome to Deckbuilder"
+  puts "1. Create a Deck"
+  puts "Option 2"
+end
+
+
+def create_a_deck(name)
+end
+
+
+
+#### Get Input ####
+
+# gets.chomp the card name
+def get_name_from_user
+  puts "please enter a card name"
+  name = gets.chomp
+  name
+end
+
 
 # Provide search options
 def choose_search
@@ -17,12 +45,6 @@ def choose_search
 
 end
 
-# gets.chomp the card name
-def get_name_from_user
-  puts "please enter a card name"
-  name = gets.chomp
-  name
-end
 
 
 ##### UNDER DEVELOPMENT #####
